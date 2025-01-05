e121e69b5b5b884da2155e1afb6efdb81a3d246d
sekar <sektech1484@gmail.com>
2025-01-05 10:50:25 +0000

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+       //create array of integers
+
+    }
+
+
+
+    private static Integer linearSearch(Integer[] nums, Integer target){
+        return -1;
+    }
+}
